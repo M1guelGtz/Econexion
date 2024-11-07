1f30ecc9b7fdafd4c6adb304dada81d53d6d0b4f
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2024-11-07 14:48:38 -0600

    Add: Card publicacion

--- src/app/Modulos/publicaciones/publicaciones.module.ts
+++ src/app/Modulos/publicaciones/publicaciones.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { PublicacionesRoutingModule } from './publicaciones-routing.module';
 import { PublicacionesComponent } from './publicaciones/publicaciones.component';
 import { LayoutComponent } from './layout/layout.component';
+import { CardPublicacionComponent } from './card-publicacion/card-publicacion.component';
 
 
 @NgModule({
   declarations: [
     PublicacionesComponent,
-    LayoutComponent
+    LayoutComponent,
+    CardPublicacionComponent
   ],
   imports: [
     CommonModule,
